033833ae5ceba89bf2d62ef7ceec4deb70e56574
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-10 17:31:10 -0300

    feat: Create a PharmacyService Interface

---
+++ src/services/IPharmacyService.ts
@@ -0,0 +1,16 @@
+export interface IPharmacyService {
+	getPharmacy(call: Record<string, any>, callback: ICallback): Promise<void>;
+	getAllPharmacies(
+		call: Record<string, any>,
+		callback: ICallback
+	): Promise<void>;
+	createPharmacy(
+		call: Record<string, any>,
+		callback: ICallback
+	): Promise<void>;
+	updatePharmacy(
+		call: Record<string, any>,
+		callback: ICallback
+	): Promise<void>;
+	deletePharmacy(call: Record<string, any>, callback: ICallback): Promise<void>;
+}
