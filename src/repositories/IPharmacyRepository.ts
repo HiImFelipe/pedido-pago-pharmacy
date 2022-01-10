e02276b86b1ea486a3266d17cb079732e26a526f
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-10 17:30:10 -0300

    fix: Update PharmacyRepository Interface

--- src/repositories/IPharmacyRepository.ts
+++ src/repositories/IPharmacyRepository.ts
@@ -2,8 +2,10 @@ import Pharmacy from '../entities/Pharmacy';
 
 export default interface IPharmacyRepository {
     getById(id: string): Promise<Pharmacy | undefined>;
+    getAll(query?: PharmacyIndexOptions): Promise<{ pharmacies: Pharmacy[]; totalPharmacies: number }>;
+    getAllByName(name: string): Promise<Pharmacy[]>;
     save(pharmacy: Pharmacy): Promise<Pharmacy>;
-    index(query: PharmacyIndexOptions): Promise<{ data: Pharmacy[]; count: number }>;
+    index(query?: PharmacyIndexOptions): Promise<{ data: Pharmacy[]; count: number }>;
     create(pharmacy: Pharmacy): Promise<Pharmacy>;
     update(id: string, pharmacy: Omit<Pharmacy, "id">): Promise<Pharmacy>;
     delete(id: string): Promise<void>;
