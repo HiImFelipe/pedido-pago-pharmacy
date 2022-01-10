7beb570c5ea4e959a367befe915450177c18c3e0
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-10 19:52:27 -0300

    feat: Create a function that returns a valid Pharmacy for testing

---
+++ src/utils/testUtils.ts
@@ -0,0 +1,20 @@
+import Pharmacy from "../entities/Pharmacy";
+
+export default class TestUtil {
+	static createAValidPharmacy(data?: Partial<Pharmacy>): Pharmacy {
+		const pharmacy = new Pharmacy();
+		pharmacy.name = data?.name || "Test Pharmacy";
+		pharmacy.address = data?.address || "Test Address";
+		pharmacy.phone = data?.phone || "(11)99234-6789";
+		pharmacy.cep = data?.cep || "12345678";
+		pharmacy.logo = data?.logo || "example/path/to/logo.png";
+		pharmacy.openingTime = data?.openingTime || "8:30";
+		pharmacy.closingTime = data?.closingTime || "17:50";
+		pharmacy.uf = data?.uf || "SP";
+		pharmacy.createdAt = data?.createdAt || new Date();
+		pharmacy.updatedAt = data?.updatedAt || new Date();
+		pharmacy.responsibleName = data?.address || "Test User";
+
+		return pharmacy;
+	}
+}
