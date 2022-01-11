968b12b86ff6103a340849241bb1fdec96219873
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-11 20:31:14 -0300

    feat: Add a production option to typeorm to run the built ts files

--- ormconfig.js
+++ ormconfig.js
@@ -10,4 +10,19 @@ const srcConfig = {
 	entities: ["./src/entities/*.ts"],
 };
 
-module.exports = srcConfig;
+const distConfig = {
+	type: "mysql",
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+	entities: ["./dist/src/entities/*.js"],
+	migrations: ["migration/*.js"],
+	cli: {
+		migrationsDir: "migration",
+	},
+};
+
+module.exports =
+	process.env.NODE_ENV === "development" ? srcConfig : distConfig;
