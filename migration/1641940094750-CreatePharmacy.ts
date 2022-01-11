44b5368413184ff5855e7eb3c893059aa7b1b2d4
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-11 20:31:41 -0300

    feat: Add CreatePharmacy migration

---
+++ migration/1641940094750-CreatePharmacy.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreatePharmacy1641940094750 implements MigrationInterface {
+    name = 'CreatePharmacy1641940094750'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`pharmacies\` (\`id\` varchar(36) NOT NULL, \`name\` varchar(255) NOT NULL, \`logo\` varchar(255) NOT NULL, \`city\` varchar(255) NOT NULL, \`uf\` varchar(255) NOT NULL, \`cep\` varchar(255) NOT NULL, \`address\` varchar(255) NOT NULL, \`phone\` varchar(255) NOT NULL, \`cnpj\` varchar(255) NOT NULL, \`openingTime\` varchar(255) NOT NULL, \`closingTime\` varchar(255) NOT NULL, \`responsibleName\` varchar(255) NOT NULL, \`isSubsidiary\` tinyint NOT NULL DEFAULT 0, \`createdAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE \`pharmacies\``);
+    }
+
+}
