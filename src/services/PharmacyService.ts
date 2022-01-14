0e0ba64220fef587d121641ef07d9b1826fd300b
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-14 09:07:58 -0300

    fix: Add a try block around all services

--- src/services/PharmacyService.ts
+++ src/services/PharmacyService.ts
@@ -14,35 +14,43 @@ export class PharmacyService implements IPharmacyService {
 		call: Record<string, any>,
 		callback: ICallback
 	): Promise<void> {
-		const { id } = call.request;
+		try {
+			const { id } = call.request;
 
-		const pharmacy = await this.pharmacyRepository.getById(id);
+			const pharmacy = await this.pharmacyRepository.getById(id);
 
-		if (!pharmacy) {
-			return callback(new Error("Pharmacy not found!"), null);
+			if (!pharmacy) {
+				return callback(new Error("Pharmacy not found!"), null);
+			}
+
+			return callback(null, {
+				...pharmacy,
+				createdAt: pharmacy.createdAt.toISOString(),
+				updatedAt: pharmacy.updatedAt.toISOString(),
+			});
+		} catch (error: any) {
+			return callback(error, null);
 		}
-
-		return callback(null, {
-			...pharmacy,
-			createdAt: pharmacy.createdAt.toISOString(),
-			updatedAt: pharmacy.updatedAt.toISOString(),
-		});
 	}
 
 	async getAllPharmacies(
 		call: Record<string, any>,
 		callback: ICallback
 	): Promise<void> {
-		const { pharmacies, totalPharmacies } =
-			await this.pharmacyRepository.getAll();
+		try {
+			const { pharmacies, totalPharmacies } =
+				await this.pharmacyRepository.getAll();
 
-		const parsedPharmacies = pharmacies.map((pharmacy) => ({
-			...pharmacy,
-			createdAt: pharmacy.createdAt.toISOString(),
-			updatedAt: pharmacy.updatedAt.toISOString(),
-		}));
+			const parsedPharmacies = pharmacies.map((pharmacy) => ({
+				...pharmacy,
+				createdAt: pharmacy.createdAt.toISOString(),
+				updatedAt: pharmacy.updatedAt.toISOString(),
+			}));
 
-		return callback(null, { pharmacies: parsedPharmacies, totalPharmacies });
+			return callback(null, { pharmacies: parsedPharmacies, totalPharmacies });
+		} catch (error: any) {
+			return callback(error, null);
+		}
 	}
 
 	async createPharmacy(
@@ -51,75 +59,86 @@ export class PharmacyService implements IPharmacyService {
 	): Promise<void> {
 		const { name } = call.request;
 
-		const pharmacyFound = await this.pharmacyRepository.getAllByName(name);
+		try {
+			const pharmacyFound = await this.pharmacyRepository.getAllByName(name);
 
-		if (pharmacyFound.length > 0) {
-			if (pharmacyFound.length > 3) {
-				return callback(new Error("Maximum subsidiaries reached"), null);
+			if (pharmacyFound.length > 0) {
+				if (pharmacyFound.length > 3) {
+					return callback(new Error("Maximum subsidiaries reached"), null);
+				}
+				const pharmacy = await this.pharmacyRepository.save(call.request);
+
+				return callback(null, {
+					...pharmacy,
+					createdAt: pharmacy.createdAt.toISOString(),
+					updatedAt: pharmacy.updatedAt.toISOString(),
+					isSubsidiary: true,
+				});
 			}
-			const pharmacy = await this.pharmacyRepository.save(call.request);
 
-			console.log({ ...pharmacy, createdAt: pharmacy.createdAt.toISOString() });
+			const pharmacy = await this.pharmacyRepository.save(call.request);
 
 			return callback(null, {
 				...pharmacy,
 				createdAt: pharmacy.createdAt.toISOString(),
 				updatedAt: pharmacy.updatedAt.toISOString(),
-				isSubsidiary: true,
 			});
+		} catch (error: any) {
+			console.log("salve");
+			return callback(error, null);
 		}
-
-		const pharmacy = await this.pharmacyRepository.save(call.request);
-
-		return callback(null, {
-			...pharmacy,
-			createdAt: pharmacy.createdAt.toISOString(),
-			updatedAt: pharmacy.updatedAt.toISOString(),
-		});
 	}
 
 	async updatePharmacy(
 		call: Record<string, any>,
 		callback: ICallback
 	): Promise<void> {
-		const { id, ...request } = call.request;
+		try {
+			const { id, ...request } = call.request;
 
-		const pharmacyFound = await this.pharmacyRepository.getById(id);
+			const pharmacyFound = await this.pharmacyRepository.getById(id);
 
-		if (!pharmacyFound) {
-			return callback(new Error("Pharmacy not found!"), null);
-		}
-
-		// Remove itens if they are falsy values (gRPC default values)
-		for (let item in request) {
-			if (!request[item]) {
-				delete request[item];
+			if (!pharmacyFound) {
+				return callback(new Error("Pharmacy not found!"), null);
 			}
+
+			// Remove itens if they are falsy values (gRPC default values)
+			for (let item in request) {
+				if (!request[item]) {
+					delete request[item];
+				}
+			}
+
+			const partialPharmacy = await this.pharmacyRepository.update(id, request);
+
+			return callback(null, {
+				...partialPharmacy,
+				createdAt: partialPharmacy?.createdAt?.toISOString() || "",
+				updatedAt: partialPharmacy?.updatedAt?.toISOString() || "",
+			});
+		} catch (error: any) {
+			return callback(error, null);
 		}
-
-		const partialPharmacy = await this.pharmacyRepository.update(id, request);
-
-		return callback(null, {
-			...partialPharmacy,
-			createdAt: partialPharmacy?.createdAt?.toISOString() || "",
-			updatedAt: partialPharmacy?.updatedAt?.toISOString() || "",
-		});
 	}
 
 	async deletePharmacy(
 		call: Record<string, any>,
 		callback: ICallback
 	): Promise<void> {
-		const { id } = call.request;
+		try {
+			const { id } = call.request;
 
-		const pharmacyFound = await this.pharmacyRepository.getById(id);
+			const pharmacyFound = await this.pharmacyRepository.getById(id);
 
-		if (!pharmacyFound) {
-			return callback(new Error("Pharmacy not found!"), null);
+			if (!pharmacyFound) {
+				return callback(new Error("Pharmacy not found!"), null);
+			}
+
+			await this.pharmacyRepository.delete(id);
+
+			return callback(null, {});
+		} catch (error: any) {
+			return callback(error, null);
 		}
-
-		await this.pharmacyRepository.delete(id);
-
-		return callback(null, {});
 	}
 }
