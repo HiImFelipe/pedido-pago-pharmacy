3ff8c317112a2fb1152b07fdc33fb04f896be84f
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-14 14:52:23 -0300

    feat: Register PharmacyProducts repository as a singleton

--- src/DI/index.ts
+++ src/DI/index.ts
@@ -1,9 +1,16 @@
 import { container } from "tsyringe";
 
 import IPharmacyRepository from "../repositories/IPharmacyRepository";
+import IPharmacyProductsRepository from "../repositories/IPharmacyProductsRepository";
 import PharmacyRepository from "../repositories/PharmacyRepository";
+import PharmacyProductsRepository from "../repositories/PharmacyProductsRepository";
 
 container.registerSingleton<IPharmacyRepository>(
 	"PharmacyRepository",
 	PharmacyRepository
+);
+
+container.registerSingleton<IPharmacyProductsRepository>(
+	"PharmacyProductsRepository",
+	PharmacyProductsRepository
 );
